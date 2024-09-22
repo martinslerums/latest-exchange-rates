bd0f4aff172e234e90b62c5dcac30dacc9e93960
martleru <68188509+martleru@users.noreply.github.com>
2024-09-22 21:53:08 +0300

    add: exchange_rate table model

---
+++ back-end/database/models/exchange_rate.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const ExchangeRate = sequelize.define('ExchangeRate', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    currency_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Currencies',
+        key: 'id',
+      },
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    exchange_rate: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  });
+
+  ExchangeRate.associate = (models) => {
+    ExchangeRate.belongsTo(models.Currency, { foreignKey: 'currency_id' });
+  };
+
+  return ExchangeRate;
+};
