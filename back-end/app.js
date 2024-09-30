f1d9315d77cdfc857c1b7ae73370ae800ec57291
martleru <68188509+martleru@users.noreply.github.com>
2024-09-30 20:11:03 +0300

    add: Added root file to run express.js server

--- back-end/app.js
+++ back-end/app.js
@@ -1,16 +1,17 @@
 require("express-async-errors");
+require("./cronJobs.js");
+
 const cors = require("cors");
 const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./database/models/index.js"); 
-require("./cronJobs.js"); 
 
+const db = require("./models/index.js");
+
+const routes = require("./routes/router.js");
 const { updateExchangeRates } = require("./cronJobs.js");
 
-// const routes = require("./routes/index.js"); // Uncomment and adjust after creating routes
-// const notFoundMiddleware = require("./middleware/not-found.js"); // Uncomment after creating middleware
-// const errorHandlerMiddleware = require("./middleware/error-handler.js"); // Uncomment after creating middleware
+const notFoundMiddleware = require("./middleware/not-found.js");
 
 dotenv.config();
 
@@ -26,32 +27,33 @@ app.use(
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send('<h1> STORE API </h1>');
-});
+app.use("/api", routes);
 
-// app.use("/api", routes); // Set up your routes here
-
-// app.use(notFoundMiddleware); // Handle 404 errors (uncomment when created)
-// app.use(errorHandlerMiddleware); // Centralized error handling (uncomment when created)
+app.use(notFoundMiddleware);
 
 const PORT = process.env.PORT || 3001;
 
 const startServer = async () => {
-  try {
-    await db.sequelize.authenticate(); 
-    console.log("Connected to SQLite successfully");
+  console.log("Connected to SQLite successfully");
 
-    // await updateExchangeRates();
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
 
+  const todayRecord = await db.CronJob.findOne({
+    where: {
+      lastRunDate: today,
+    },
+  });
 
-    app.listen(PORT, () =>
-      console.log(`Server is listening on http://localhost:${PORT}`)
-    );
-  } catch (error) {
-    console.error("Failed to connect to the database:", error.message);
-    process.exit(1); 
+  if (!todayRecord) {
+    await updateExchangeRates();
+  } else {
+    console.log("Today's exchange rates have already been updated.");
   }
+
+  app.listen(PORT, () =>
+    console.log(`Server is listening on http://localhost:${PORT}`)
+  );
 };
 
 startServer();
