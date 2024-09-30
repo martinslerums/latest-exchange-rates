2fc80be1fd22fb875badba944efc692d41b3df0f
martleru <68188509+martleru@users.noreply.github.com>
2024-09-30 20:06:04 +0300

    database

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -1,30 +1,29 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const { env } = require('process');
-
+const process = require('process');
 const basename = path.basename(__filename);
-const config = require('../../config/config.json')[env.NODE_ENV || 'development'];
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  const dbPath = path.join(__dirname, '../../', config.database);
-  console.log('Database path:', dbPath); 
-  sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: dbPath,
-  });
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs.readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js'
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
     );
   })
   .forEach(file => {
