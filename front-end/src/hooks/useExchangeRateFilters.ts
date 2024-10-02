58def0f8758ba215395e617e62521118b9d5fd47
martleru <68188509+martleru@users.noreply.github.com>
2024-10-02 16:17:21 +0300

    feat: Hook controls limit and page params sent to BE

--- front-end/src/hooks/useExchangeRateFilters.ts
+++ front-end/src/hooks/useExchangeRateFilters.ts
@@ -1,35 +1,36 @@
 import { CurrencyFilters } from "@/types/typings";
-import { useCallback, useEffect } from "react";
+import { useCallback } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const useExchangeRateFilters = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
-    const currentTargetCurrency = searchParams.get("target_currency");
-    if (!currentTargetCurrency) {
-      setSearchParams({ target_currency: "USD" });
-    }
-  }, [searchParams, setSearchParams]);
-
   const filters: CurrencyFilters = {
-    target_currency: searchParams.get("target_currency") as CurrencyFilters['target_currency']
+    target_currency: searchParams.get("target_currency") as CurrencyFilters['target_currency'],
+    page: searchParams.get("page") as CurrencyFilters['page'] || undefined,
+    limit: searchParams.get("limit") as CurrencyFilters['limit'] || undefined,
+    order: searchParams.get('order') as CurrencyFilters['order']
   };
 
   const setFilters = useCallback(
     (filters: Partial<CurrencyFilters>) => {
       setSearchParams((params) => {
         Object.entries(filters).forEach(([key, value]) => {
-          params.set(key, String(value));
+          if ((key === 'page' || key === 'limit') && !isNaN(Number(value))) {
+            params.set(key, String(value));
+          }
+          
+          if (key !== 'page' && key !== 'limit') {
+            params.set(key, String(value));
+          }
         });
-        
         return params;
       });
     },
     [setSearchParams]
   );
 
-  return { ...filters, setFilters};
+  return { ...filters, setFilters };
 };
 
 export default useExchangeRateFilters;
