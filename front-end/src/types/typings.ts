e3c57712d4cde204be048b4958bd3169cf5f5a80
martleru <68188509+martleru@users.noreply.github.com>
2024-10-02 16:16:47 +0300

    add: Added additional typings

--- front-end/src/types/typings.ts
+++ front-end/src/types/typings.ts
@@ -12,15 +12,27 @@ export type APIResponse = {
   highest: number;
   lowest: number;
   average: number;
+  lastUpdate: string;
+  pagination: Pagination;
 };
 
 export type Statistics = {
   highest: number;
   lowest: number;
   average: number;
-  date?: string
+  date?: string;
 };
 
 export type CurrencyFilters = {
   target_currency: "USD" | "AUD" | "GBP";
+  page?: string;
+  limit?: string;
+  order?: "ASC" | "DESC";
+};
+
+export type Pagination = {
+  page: number;
+  limit: number;
+  totalRecords: number;
+  totalPages: number;
 };
