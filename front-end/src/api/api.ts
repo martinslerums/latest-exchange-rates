97c4a6de43dd993691432abf1c318089bf651a2e
martleru <68188509+martleru@users.noreply.github.com>
2024-10-02 16:16:30 +0300

    add: Custom API

--- front-end/src/api/api.ts
+++ front-end/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const customApi = axios.create({
   baseURL: "http://localhost:3001/api",
-  timeout: 10000,
+  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
